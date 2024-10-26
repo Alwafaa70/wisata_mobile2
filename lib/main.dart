ecbe9c54716acacbadcf450f3a04fdcedab5f054
alwafaa70 <m.alwafa.k@gmail.com>
2024-10-26 13:54:15 +0700

    commit 2

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:alwafaa_travel/latihanModul/pertemuan4.dart';
+import 'package:alwafaa_travel/screen/splashscreen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(TravelApp());
+}
+
+class TravelApp extends StatelessWidget {
+  const TravelApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    );
+  }
+}
