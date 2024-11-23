0e060a69a48213065ef4c8f44daee7c7db6b5d98
alwafaa70 <m.alwafa.k@gmail.com>
2024-11-23 13:49:57 +0700

    pertemuan7

--- lib/screen/homepage.dart
+++ lib/screen/homepage.dart
@@ -1,5 +1,7 @@
 import 'package:alwafaa_travel/cons.dart';
 import 'package:alwafaa_travel/model/destination_model.dart';
+import 'package:alwafaa_travel/widgets/popular_destination.dart';
+import 'package:alwafaa_travel/widgets/rekomendasi_destination.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,12 +12,174 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int selectedPage = 0;
+
+  List<IconData> icons = [
+    Icons.home_filled,
+    Icons.bookmark_border_rounded,
+    Icons.shopping_cart_outlined,
+    Icons.person_outline_outlined,
+  ];
+
+  List<TravelDestination> popular = listDestination
+      .where((element) => element.category == 'popular')
+      .toList();
+
+  List<TravelDestination> rekomendasi = listDestination
+      .where((element) => element.category == 'rekomendasi')
+      .toList();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: kBackgroundColor,
       body: Column(
-        children: [BuildAppBar()],
+        children: [
+          BuildAppBar(),
+          SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 15),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Destinasi Populer',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.black,
+                  ),
+                ),
+                Text(
+                  'Lihat Semua',
+                  style: TextStyle(
+                    fontSize: 12,
+                    fontWeight: FontWeight.w600,
+                    color: blueTextColor,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            padding: EdgeInsets.only(left: 15),
+            child: Row(
+              children: List.generate(
+                popular.length,
+                (index) => Padding(
+                  padding: EdgeInsets.only(right: 15),
+                  child: GestureDetector(
+                    onTap: () {},
+                    child: PopularDestination(
+                      destination: popular[index],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 15),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Destinasi Populer',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.black,
+                  ),
+                ),
+                Text(
+                  'Lihat Semua',
+                  style: TextStyle(
+                    fontSize: 12,
+                    fontWeight: FontWeight.w600,
+                    color: blueTextColor,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Expanded(
+            child: SingleChildScrollView(
+              scrollDirection: Axis.vertical,
+              padding: EdgeInsets.symmetric(horizontal: 15),
+              child: Column(
+                children: List.generate(
+                  rekomendasi.length,
+                  (index) => Padding(
+                    padding: EdgeInsets.only(bottom: 15),
+                    child: GestureDetector(
+                      onTap: () {},
+                      child: RekomendasiDestination(
+                          destination: rekomendasi[index]),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 15),
+                  child: Column(
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 15, vertical: 18),
+                        decoration: BoxDecoration(
+                          color: kButtonColor,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: List.generate(
+                            icons.length,
+                            (index) => GestureDetector(
+                              onTap: () {
+                                setState(() {
+                                  selectedPage = index;
+                                });
+                              },
+                              child: Icon(
+                                icons[index],
+                                size: 32,
+                                color: selectedPage == index
+                                    ? Colors.white
+                                    : Colors.white.withOpacity(0.4),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 25,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
